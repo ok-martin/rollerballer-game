42d2cd2e9daf8a2026c91ef7417dbb0ff580de3a
okkool <emirt6@gmail.com>
2018-02-08 23:10:02 +0800

    Mesh Gen: Marching Squares

--- Assets/scripts/MapGenerator.cs
+++ Assets/scripts/MapGenerator.cs
@@ -45,6 +45,10 @@ public class MapGenerator : MonoBehaviour
         {
             SmoothMap();
         }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        float size = 1;
+        meshGen.GenerateMesh(map, size);
     }
 
     // Create the map based on the seed. If feed the same seed, it will generate the same map
@@ -127,18 +131,18 @@ public class MapGenerator : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        if (map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
+         /*if (map != null)
+         {
+             for (int x = 0; x < width; x++)
+             {
+                 for (int y = 0; y < height; y++)
+                 {
+                     Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                     Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                     Gizmos.DrawCube(pos, Vector3.one);
+                 }
+             }
+         }*/
     }
 
 }
