da8276a0f2122fc313be30057a0323675a71ee6d
okkool <emirt6@gmail.com>
2018-02-08 14:33:08 +0800

    working basic game (initial commit)

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // The reference to the ball
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Initialization
+    void Start ()
+    {
+        // Place this behind the player
+        offset = transform.position - player.transform.position;
+    }
+
+    // For last known states. Run after all items have been processed + updated
+    void LateUpdate()
+    {
+        // Update the camera after the player has moved
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    // Use rigidbody for ball physics
+    private Rigidbody ballBody;
+    private int count;
+
+    // Initialization
+    void Start ()
+    {
+        ballBody = GetComponent<Rigidbody>();
+        count = 8;
+        SetCountText();
+        winText.text = "";
+    }
+	
+	// Update is called once per frame (before rendering a frame)
+	void Update () { }
+
+    // Called just before any physics calcs
+    void FixedUpdate()
+    {
+        // Get current position x and y
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        // Create the x,y vector
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Push the ball
+        ballBody.AddForce(movement * speed);
+    }
+
+    // When player object first touches an object collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if it is the pickup object.
+        // Also: Add rigidbody to pickup object. Make it Kinematic.
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            // Hide the pickup as opposed to Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+
+            // Update the count
+            count--;
+            SetCountText();
+        }
+    }
+
+    // Update the text UI
+    void SetCountText()
+    {
+        countText.text = "Collect: " + count.ToString();
+        if (count == 0)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
