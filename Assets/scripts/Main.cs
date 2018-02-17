9fe0690cbaf3ed51ce850e70b48e4a3941861e3b
okkool <emirt6@gmail.com>
2018-02-17 15:39:06 +0800

    Map to World Coordinates function & fix include scale
    Map2WorldCoordinates

--- Assets/scripts/Main.cs
+++ Assets/scripts/Main.cs
@@ -6,8 +6,13 @@ public class Main : MonoBehaviour
 {
     public GameObject player;
 
-	// Use this for initialization
-	void Start ()
+    private int mapWidth = 0;
+    private int mapHeight = 0;
+    private float mapScale = 1;
+    private int[,] map;
+
+    // Use this for initialization
+    void Start ()
     {
         // Access the map generator
         MapGenerator mpg = GetComponent<MapGenerator>();
@@ -15,29 +20,26 @@ public class Main : MonoBehaviour
         // Create the world
         mpg.Main();
 
-        int[,] map = mpg.map;
-        int width = mpg.width;
-        int height = mpg.height;
-        float scale = mpg.scale;
+        map = mpg.map;
+        mapWidth = mpg.width;
+        mapHeight = mpg.height;
+        mapScale = mpg.scale;
 
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < mapWidth; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < mapHeight; y++)
             {
                 // if first occurance of clear region
                 if(map[x,y] == 0)
                 {
                     // convert into world coordinates
-                    int halfx = width / 2 - 1;
-                    int halfy = height / 2 - 1;
-                    float px = x > halfx ? x - halfx : 0 - (halfx - x);
-                    float py = y > halfy ? y - halfy : 0 - (halfy - y);
+                    float[] pos = Map2WorldCoordinates(x, y);
 
-                    // Move player to a starting location
-                    player.transform.position = new Vector3(px, 0F, py);
+                    // move player to a starting location
+                    player.transform.position = new Vector3(pos[0], 0F, pos[1]);
                     
                     // break
-                    x = width;
+                    x = mapWidth;
                     break;
                 }
             }
@@ -46,10 +48,26 @@ public class Main : MonoBehaviour
         
     }
 	
-    
+    /*  Given map position (x and y) convert into unity world
+     *  coordinates / position assuming the world is build from origin.
+     *  Include scale calculation.
+     *  Map[x,y] starts at -x, -z location in Unity
+     */
+    private float[] Map2WorldCoordinates(int x, int y)
+    {
+        int halfx = mapWidth / 2 - 1;
+        int halfy = mapHeight / 2 - 1;
 
-	// Update is called once per frame
-	void Update ()
+        return new float[]
+        {
+            (x > halfx ? x - halfx : 0 - (halfx - x)) * mapScale,
+            (y > halfy ? y - halfy : 0 - (halfy - y)) * mapScale
+        };
+    }
+
+
+    // Update is called once per frame
+    void Update ()
     {
 		
 	}
